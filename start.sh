83169ff4e0882afb5a10433a164449dad05b94db
setting <hosting@gmail.com>
2022-08-27 16:49:47 -0300

    Firsh commit

--- start.sh
+++ start.sh
@@ -34,28 +34,41 @@ fi
 echo '\033[1;33mSetting "git" wait...'
 sleep 2
 read -p 'The current directory will be hosted... do you agree? [Y/N default "Y"]: ' accept
-case $accept in "N" | "n") exit;; esac
+case $accept in "N" | "n") sleep 2; echo "Open the current directory."; exit;; esac
 if [ -d "$(pwd)/.git" ]; then
 read -p 'The ".git" folder was detected. Remove this file? [Y/N default "Y"]: ' accept
-case $accept in "Y" | "y") rm -rf .git;; *) sleep 2;; esac
+case $accept in "Y" | "y") rm -rf .git; git init;; *) sleep 2;; esac
 else
 sleep 2
 fi
 echo "Proceeding, setting git config."
 sleep 2
-git config --global user.email "hosting@gmail.com"
-git config --global user.name "setting"
-path=$(pwd)
+if [ -d "$(pwd)/.git" ]; then
+echo "git detected"
+git add ./
 git config --global --add safe.directory /storage/emulated/0/${path#*/sdcard/}
-git init
-git add .
 git commit -am "Firsh commit"
 read -p "Write a name. By default one will be generated automatically: " name
 if [ -z $name ]; then
-echo "$(date +%H:%M:%S)"
 gh repo create $(date +%H.%M.%S) --push --public --source ./
 else
 gh repo create $name --push --public --source ./
 sleep 2
+fi
+echo "Succesfully"
+else
+git config --global user.email "hosting@gmail.com"
+git config --global user.name "setting"
+path=$(pwd)
+git config --global --add safe.directory /storage/emulated/0/${path#*/sdcard/}
+git add ./
+git commit -am "Firsh commit"
+read -p "Write a name. By default one will be generated automatically: " name
+if [ -z $name ]; then
+gh repo create $(date +%H.%M.%S) --push --public --source ./
+else
+gh repo create $name --push --public --source ./
+sleep 2
+fi
 echo "Succesfully"
 fi
